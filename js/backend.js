8100d75be520a219030f7eeb8ffb607d70832f5d
mas5592 <mas5592@yandex.ru>
2019-10-29 19:45:56 +0300

    Работа с сетью

--- js/backend.js
+++ js/backend.js
@@ -35,7 +35,7 @@
       onError('Произошла ошибка соединения');
     });
     xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      onError('Запрос не смог быть выполнен за ' + xhr.timeout + 'мс');
     });
 
     xhr.timeout = 1000;
